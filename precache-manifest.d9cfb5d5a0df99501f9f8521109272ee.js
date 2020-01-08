eabaf5de085a99a34b03d3febf40f3dd447c8904
Brahian <teslasapbe@gmail.com>
2020-01-07 21:13:25 -0300

    Updates

---
+++ precache-manifest.d9cfb5d5a0df99501f9f8521109272ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a53f35dea2b5e00911414551cf3a22e",
+    "url": "/react-ecommerce/index.html"
+  },
+  {
+    "revision": "efbc208e6b99bced8e11",
+    "url": "/react-ecommerce/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "92946774df9a2278ae0e",
+    "url": "/react-ecommerce/static/js/2.4132db81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-ecommerce/static/js/2.4132db81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efbc208e6b99bced8e11",
+    "url": "/react-ecommerce/static/js/main.6669b95d.chunk.js"
+  },
+  {
+    "revision": "3a1483193ff7433148d7",
+    "url": "/react-ecommerce/static/js/runtime-main.3eb045eb.js"
+  }
+]);
