5fb469fd5b0ae8ea20be2e2a3c4e1ab74867af89
Brahian <teslasapbe@gmail.com>
2020-02-15 22:54:49 -0300

    added selectors and have to fix remove item from checkout

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 class App extends React.Component {
   unSubscribeFromAuth = null;
@@ -53,6 +54,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,20 @@ export const addItemToCart = (cartItems, newItem) => {
 
   return [...cartItems, { ...newItem, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === itemToRemove.id
+  );
+
+  if (existingCartItem == 1) {
+    //removes when the condition evaluates to false
+    return cartItems.filter(cartItem => cartItem.id != itemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id == itemToRemove.id
+      ? { ...cartItems, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
